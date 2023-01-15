c3b7110fed137d66c6048d7023c3929f1a4a67b5
Matuzalane <calebcastilhonunes@gmail.com>
2023-01-14 21:02:55 -0300

    Criando projeto de E-Commerce - Velho ALtair com ASP.NET CORE MVC 7.0

---
+++ Controllers/PedidosController.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using velho_altair.Models;
+
+
+namespace velho_altair.Controllers;
+
+public class PedidosController : Controller
+{
+    
+}
