57bc9579c408c4e8ae999c9a40cacfc0db030506
Matuzalane <calebcastilhonunes@gmail.com>
2023-01-14 21:05:01 -0300

    Criando projeto de E-Commerce - Velho ALtair com ASP.NET CORE MVC 7.0

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using velho_altair.Models;
+
+namespace velho_altair.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
